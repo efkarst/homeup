1d43533b95a8e6ccd2c223b76b4632f138dfbd4a
Elizabeth <efkarst@outlook.com>
2018-12-09 17:06:10 -0800

    Add memoization to store current_user helper method value

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      User.find(session[:user_id])
+      current_user ||= User.find(session[:user_id])
     end
   end
 
