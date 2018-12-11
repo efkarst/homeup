4b636b34b89ede8f7e5057ca5ad03f481f3f6554
Elizabeth <efkarst@outlook.com>
2018-12-10 18:36:17 -0800

    Add project details to side bar on project, room, and user show pages

--- app/models/room.rb
+++ app/models/room.rb
@@ -9,4 +9,28 @@ class Room < ActiveRecord::Base
   def self.find_by_slug(slug)
     self.find_by({name: slug.split("-").join(" ")})
   end
+
+  def total_cost
+    cost = 0
+    self.projects.each do |project|
+      cost += project.cost.to_i
+    end
+    cost
+  end
+
+  def total_duration
+    duration = 0
+    self.projects.each do |project|
+      duration += project.duration.to_i
+    end
+    duration
+  end
+
+  def number_of_projects_completed
+    completed = 0
+    self.projects.each do |project|
+      completed += 1 if project.status == "Complete"
+    end
+    completed
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,28 @@ class User < ActiveRecord::Base
     self.find_by({username: slug.split("-").join(" ")})
   end
 
+  def total_cost
+    cost = 0
+    self.projects.each do |project|
+      cost += project.cost.to_i
+    end
+    cost
+  end
+
+  def total_duration
+    duration = 0
+    self.projects.each do |project|
+      duration += project.duration.to_i
+    end
+    duration
+  end
+
+  def number_of_projects_completed
+    completed = 0
+    self.projects.each do |project|
+      completed += 1 if project.status == "Complete"
+    end
+    completed
+  end
+
 end
