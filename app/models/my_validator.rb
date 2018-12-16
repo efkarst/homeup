06e526d4bc199e86134877d9fd837537a3190a94
Elizabeth <efkarst@outlook.com>
2018-12-16 10:54:19 -0800

    Refactor MyValidator class to make code more readable

--- app/models/my_validator.rb
+++ app/models/my_validator.rb
@@ -1,12 +1,20 @@
 class MyValidator < ActiveModel::Validator
   def validate(record)
-    if user_item_names(record).include?(record.name.downcase) && User.find_by_slug(:username, record.user.slug(:username)).send(item_plural(record)).find_by_slug(:name, record.slug(:name)).id != record.id
+    if user_already_has_item_with_name?(record) && user_item_is_not_current_item(record)
       record.errors[:base] << "You already have a #{record.class.name.downcase} named '#{record.name}'"
     end
   end
 
+  def user_already_has_item_with_name?(record)
+    user_item_names(record).include?(record.name.downcase)
+  end
+
   def user_item_names(record)
     record.user.send(item_plural(record)).all.collect { |item| item.name }
+  end
+
+  def user_item_is_not_current_item(record)
+    User.find_by_slug(:username, record.user.slug(:username)).send(item_plural(record)).find_by_slug(:name, record.slug(:name)).id != record.id
   end
 
   def item_plural(record)
