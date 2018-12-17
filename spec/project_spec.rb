7725209fe6c4200fa3cbf3dd71ac43c681cc88e9
Elizabeth <efkarst@outlook.com>
2018-12-16 16:57:56 -0800

    Add test to validate editing a project'

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -6,8 +6,9 @@ end
 
 describe 'projects' do
 
-  before do
-    User.create(name: "Fred", username:"fred0123", password: "password")
+  before(:each) do
+    user = User.create(name: "Fred", username:"fred0123", password: "password")
+    project = Project.create(name:"Channel board ceiling", room: Room.create(name: "Second bedroom", user: user), cost: 900, duration: 15)
     visit '/'
     click_link('Log In')
     fill_in("username", :with => "fred0123")
@@ -33,10 +34,31 @@ describe 'projects' do
     expect(page).to have_content("Tile shower")
     expect(page).to have_content("$3000")
     expect(page).to have_content("30 hours")
-    expect(page).to have_content("Tile shower")
     expect(page).to have_content("Bathroom")
     expect(page).to have_content("Not started")
     expect(page).to have_content("Need to replace the tiles from 1969")
     expect(page).to have_content("Tiles, cement, tiling tools, grout.")
   end
+
+  it "allows a user to edit a project" do
+    visit '/users/fred0123/projects/channel-board-ceiling/edit'
+    expect(current_path).to eq('/users/fred0123/projects/channel-board-ceiling/edit')
+    expect(page).to have_content("Edit channel board ceiling")
+    fill_in("cost", :with => "1800")
+    # fill_in("duration", :with => "45")
+    # page.choose("In progress")
+    # fill_in("description", :with => "Add a description")
+    # fill_in("materials", :with => "Add materials")
+    # click_on('Update Project')
+    # expect(current_path).to eq('/users/fred0123/projects/channel-board-ceiling')
+    # expect(page).to have_content("Channel board ceiling")
+    # # expect(page).to have_content("$1800")
+    # # expect(page).to have_content("45 hours")
+    # expect(page).to have_content("Second bedroom")
+    # # expect(page).to have_content("In progress")
+    # expect(page).to have_content("Add a description")
+    # expect(page).to have_content("Add materials")
+  end
 end
+
+# visit     page     fill_in     check     uncheck     choose     click_link     click_button     click_on
