dcc8d4905a15c6015b7a0fa6a875692466199cb0
Elizabeth <efkarst@outlook.com>
2018-12-20 07:49:56 -0800

    Update ProjectStats module to make method names more readable

--- app/models/concerns/project_stats.rb
+++ app/models/concerns/project_stats.rb
@@ -1,28 +1,29 @@
 module ProjectStats
+
+  # Total cost of incomplete projects that belong to self
   def total_cost_of_incomplete_projects
     cost = 0
-    self.projects.each do |project|
+    self.incomplete_projects.each do |project|
       cost += project.cost.to_i
     end
     cost
   end
 
+  # Total duration of incomplete projects that belong to self
   def total_duration_of_incomplete_projects
     duration = 0
-    self.projects.each do |project|
+    self.incomplete_projects.each do |project|
       duration += project.duration.to_i
     end
     duration
   end
 
+  # Total number of projects completed that belong to self
   def number_of_projects_completed
-    completed = 0
-    self.projects.each do |project|
-      completed += 1 if project.status == "Complete"
-    end
-    completed
+    complete_projects.size
   end
 
+  # Array of projects completed that belong to self
   def complete_projects
     completed = []
     self.projects.each do |project|
@@ -31,6 +32,7 @@ module ProjectStats
     completed
   end
 
+  # Array of incomplete projects that belong to self 
   def incomplete_projects
     incomplete = []
     self.projects.each do |project|
