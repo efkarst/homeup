993fc99708c1eafd2833d35506abfcf6c934aff4
Elizabeth <efkarst@outlook.com>
2018-12-20 19:38:19 -0800

    Add description to methods in slug.rb

--- app/models/concerns/slug.rb
+++ app/models/concerns/slug.rb
@@ -1,11 +1,13 @@
 module Slug
   module InstanceMethods
+    # Instance method to get the slug for an object property
     def slug(property)
       self[property].split(" ").join("-").downcase
     end
   end
 
   module ClassMethods
+    # Class method to find an object based on the slug of a property
     def find_by_slug(property, slug)
       self.find_by({property => slug.split("-").join(" ")})
     end
